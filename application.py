ed33f04a9a1f7290e6be5c31cc6b442582f4e971
gwijeratne <32789848+gwijeratne@users.noreply.github.com>
2017-11-07 20:23:24 -0700

    Add files via upload

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 
 # Connect to Database and create database session
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('postgresql://catalog:catalog@localhost:5432/catalog')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
